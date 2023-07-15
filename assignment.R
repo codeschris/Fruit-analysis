7a863230c0f9f5952ac99898ead9f7be8a9808a2
codeschris <chrismlolwa42@outlook.com>
2023-07-15 20:22:00 +0300

    Additions - Correlations between selected variables was done - Independent and dependent T tests have been performed for chosen variables - Plotting of graph

--- assignment.R
+++ assignment.R
@@ -1,7 +1,4 @@
-#Data visualizations
-#descriptive statistics for relevant variables
-#independent and paired t-tests
-
+#Importing libraries
 library(ggplot2)
 library(dplyr)
 
@@ -13,9 +10,10 @@ str(data)
 
 #defining variables
 yield = data$Yield
-price = data$RetailPrice
-form_pkg = unique(data$Fruit)
+cup_size = data$CupEquivalentSize
+cup_price = data$CupEquivalentPrice
 retail_price = data$RetailPrice
+
 count_form = data %>% count(data$Form)
 count_form
 
@@ -36,11 +34,11 @@ range(yield)
 #retail price
 range(retail_price)
 
-df <- as.data.frame(count_form)
-df
+df1 <- as.data.frame(count_form)
+df1
 
 #Number of fruits available according to their forms
-ggplot(df, aes(x = `data$Form`, y = n)) +
+ggplot(df1, aes(x = `data$Form`, y = n)) +
   geom_bar(stat = "identity") +
   ylim(0, 30) +
   xlab("Form of fruit") +
@@ -48,7 +46,46 @@ ggplot(df, aes(x = `data$Form`, y = n)) +
   ggtitle("Count of the forms of fruit") +
   geom_text(aes(label = n), vjust = -0.5)
 
-#Checking trend of retail price and yield
+#Checking mean Retail price of form category
+df2 <- df %>%
+  group_by(`data$Form`) %>%
+  summarise(price = mean(data$RetailPrice[data$Form == first(`data$Form`)]))
 
+ggplot(df2, aes(x = `data$Form`, y = price)) +
+  geom_bar(stat = "identity") +
+  ylab("Price") +
+  xlab("Fruit Form") +
+  ggtitle("Mean Price by Fruit Form")
 
-        
+#Correlations
+cor_price <- cor(yield, retail_price)
+cor_size <- cor(retail_price, cup_size)
+cor_yield <- cor(cup_size, yield)
+cor_cupPrice <- cor(yield, cup_price)
+
+#Correlation results
+cor_cupPrice
+cor_yield
+cor_size
+cor_price
+
+#T Tests
+#Independent
+ind_result1 <- t.test(yield, retail_price)
+ind_result2 <- t.test(cup_size, cup_price)
+ind_result3 <- t.test(retail_price, cup_price)
+
+#Results
+ind_result1
+ind_result2
+ind_result3
+
+#Dependent
+dep_result1 <- t.test(yield, retail_price, paired=TRUE)
+dep_result2 <- t.test(cup_size, cup_price, paired=TRUE)
+dep_result3 <- t.test(retail_price, cup_price, paired=TRUE)
+
+#Results
+dep_result1
+dep_result2
+dep_result3
